3c62431f9defb3552b12589e4631d90730ca9e96
edri4s <edri.adisetiawan@gmail.com>
2024-06-15 14:47:54 +0800

    fix(connect db):perbaikan env untuk konek database

--- initialize/setup.go
+++ initialize/setup.go
@@ -2,15 +2,29 @@ package initialize
 
 import (
 	"Matahariled/models"
+	"fmt"
+	"os"
 
+	"github.com/subosito/gotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
+func GetDSN() string {
+	gotenv.Load()
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, pass, host, dbName)
+}
+
 func ConnectDatabase() {
-	db, err := gorm.Open(mysql.Open("root:@tcp(localhost:3306)/matahariled?parseTime=true"))
+	dsn := GetDSN()
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
